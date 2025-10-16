4ad97720e4bd4922d729823d7d3881aff056ad89
Avelanda <gorthell@gmail.com>
2025-10-16 15:07:30 +0200

    Update main.py
    Executing modular properties of a granular model through addressing the mainframe control of functional calls.

--- Tello_Video/main.py
+++ Tello_Video/main.py
@@ -1,3 +1,6 @@
+# Copyright © 2025 "Avelanda".
+# All rights reserved.
+
 import tello
 from tello_control_ui import TelloUI
 
@@ -12,3 +15,20 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+def MainSet():
+ drone = drone
+ if drone != 0:
+  vplayer = vplayer
+  if vplayer != 0:
+   return 0
+  
+ while vplayer.root.mainloop() *True or False:
+  vaplayer.root.mainloop() is not drone
+  vaplayer.root.mainloop() is not vplayer
+  return 0
+  
+  for __name__ in (0) or (1):
+   while main():
+    return 0
+MainSet()
